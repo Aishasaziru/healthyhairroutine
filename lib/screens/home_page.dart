b164004b667a2c3bf384fb45c47100616ce29b37
Aishasaziru <aishailhansa@gmail.com>
2023-02-03 17:33:46 +0300

    adding my project to git

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,52 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //APPBAR
+      appBar: AppBar(
+        title: const Text("Healthy Hair Routine"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+          )
+        ],
+      ),
+      //MAIN CONTENT
+      body: Container(
+        child: ListView(
+          children: [
+            //IMAGE
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * 0.3,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("images/blondyhair.jpg"),
+                      fit: BoxFit.cover)),
+            ),
+            //SPACE
+            SizedBox(
+              height: 20,
+            ),
+            ListTile(
+              title: Text("home remedies for hair"),
+              subtitle: Text("by aisha "),
+            ),
+          
+          ],
+        ),
+        
+      ),
+    );
+  }
+}
