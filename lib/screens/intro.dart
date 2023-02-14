f303c3a3dcf07787cdeb1b7c14a9b7aa0b3f8c70
Aishasaziru <aishailhansa@gmail.com>
2023-02-14 15:22:22 +0300

    working on some errors

--- lib/screens/intro.dart
+++ lib/screens/intro.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// ignore: camel_case_types
 class intro extends StatelessWidget {
   const intro({super.key});
 
@@ -16,13 +17,17 @@ class intro extends StatelessWidget {
               //FIRST CONTAINER
               Container(
                 width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.width * 0.022,
+                height: MediaQuery.of(context).size.width * 0.22,
                 decoration: BoxDecoration(
                   image: DecorationImage(
-                      image: AssetImage(
-                        "images/blondyhair.jpg",
-                      ),
-                      fit: BoxFit.cover),
+                    image: AssetImage(
+                      "images/blondyhair.jpg",
+                    ),
+                    fit: BoxFit.cover,
+                    colorFilter: ColorFilter.mode(
+                        Colors.pinkAccent.withOpacity(0.5), BlendMode.darken),
+                  ),
+                  borderRadius: BorderRadius.circular(20),
                 ),
               ),
             ],
