7f684dc50884859dd2bc837bd19105fc0e432c89
Aisha Saziru <aishailhansa@gmail.com>
2023-05-12 18:15:57 +0300

    pushing with fhub-1

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,7 +1,8 @@
+// ignore_for_file: prefer_typing_uninitialized_variables
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:my_flutter_project/screens/intro.dart';
 
 import '../Utils/utils.dart';
